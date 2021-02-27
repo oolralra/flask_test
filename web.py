e8bae0ce92f178838d1cbaaa4b355ce297ee2fe9
oolralra <oolralra@gmail.com>
2021-02-27 19:02:16 +0900

    Update web.py

--- web.py
+++ web.py
@@ -1,7 +1,12 @@
-echo "# flask_test" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/oolralra/flask_test.git
-git push -u origin main
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
